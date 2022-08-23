e897f5d8b143d79d66fedd6177cfa4178659218b
ppanticona <ppanticona@gmail.com>
2022-08-23 10:40:49 -0500

    Initial version of fabio generated by generator-jhipster@7.9.2

---
+++ src/test/java/com/ppanticona/fabio/IntegrationTest.java
@@ -0,0 +1,22 @@
+package com.ppanticona.fabio;
+
+import com.ppanticona.fabio.FabioApp;
+import com.ppanticona.fabio.config.AsyncSyncConfiguration;
+import com.ppanticona.fabio.config.EmbeddedMongo;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
+
+/**
+ * Base composite annotation for integration tests.
+ */
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+@SpringBootTest(classes = { FabioApp.class, AsyncSyncConfiguration.class })
+@EmbeddedMongo
+@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_CLASS)
+public @interface IntegrationTest {
+}
